9537dfaee17b6de91784f7e80c65c01c1266f2e7
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2024-06-11 19:45:35 -0300

    feat: change route architecture

---
+++ src/routes/users.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const usersRoutes = express.Router(); 
+
+usersRoutes.post("/", (request, response) => {
+    const { name, email, password } = request.body;
+    
+    response.json({ name, email, password });
+});
+
+module.exports = usersRoutes;

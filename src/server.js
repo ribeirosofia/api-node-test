8906b2ed4ce87e68d878b71d48d254893ff372f0
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2024-06-11 16:02:02 -0300

    feat: insert route params

--- src/server.js
+++ src/server.js
@@ -2,8 +2,13 @@ const express  = require('express');
 
 const app = express();
 
-app.get("/message", (request, response) => {
-    response.send("Hellooo world!");
+app.get("/message/:id/:user", (request, response) => {
+    const {id, user} = request.params;
+
+    response.send(`
+        Mensagem: ${id}.
+        Para o usuário: ${user}.
+        `);
 })
 
 const PORT = 3333;

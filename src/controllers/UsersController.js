3451b52c2ac86c01e0907649a0abdf53b5cb8482
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2024-06-12 18:34:24 -0300

    feat: insert new user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -3,16 +3,19 @@ const sqliteConnection = require('../database/sqlite');
 
 class UsersController {
     async create(request, response) {
-      const { name, email, password } = request.body;
-      const database = await sqliteConnection();
-      const checkUserExists = await database.
-      get("SELECT * FROM users WHERE email = (?)", [email]);
+    const { name, email, password } = request.body;
+    const database = await sqliteConnection();
+    const checkUserExists = await database.
+    get("SELECT * FROM users WHERE email = (?)", [email]);
 
-      if(checkUserExists){
-        throw new AppError("Este e-mail já está cadastrado.");
-      }
-      return response.status(201).json();
+    if(checkUserExists){
+      throw new AppError("Este e-mail já está cadastrado.");
     }
+    await database.run("INSERT INTO users (name, email, password) VALUES(?,?, ?)",
+      [name, email, password]
+    ); 
+    return response.status(201).json();
   }
+}
   
   module.exports = UsersController;
